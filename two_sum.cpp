a5bf47b3f1fc859c1b8ccecafa430c060ba54158
aakash <sachin@sachins-MacBook-Air.local>
2025-04-12 15:20:38 +0530

    Initial commit

---
+++ two_sum.cpp
@@ -0,0 +1,19 @@
+class Solution {
+    public:
+        vector<int> twoSum(vector<int>& nums, int target) {
+            int n = nums.size();
+    
+            map<int , int> mp;
+    
+            for(int i = 0; i < n; i++){
+                int remEle = target - nums[i];
+                if(mp.count(remEle)){//element hai 
+                    return {mp[remEle] , i};
+                }
+                //agar nhi hai to map me dal denge current element ko 
+                mp[nums[i]] = i;//map me element or index dono save kr rhe hai
+            }
+    
+            return {};//yhan tk nhi pahuchega kyonki answer exist krta hia
+        }
+    };
