39e8b709d919756b181cd31c00fc5f3c2ec76d48
optimisticaakash <130359837+optimisticaakash@users.noreply.github.com>
2025-05-22 12:50:10 +0530

    Create Leetcode509_fibonaccinumber.cpp

---
+++ Leetcode509_fibonaccinumber.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int fib(int n) {
+        if(n<= 1) return n;
+        int last = fib(n-1);
+        int slast = fib(n-2);
+
+        return last+slast;
+    }
+};
+
+//time complexity = O(2^n) //more than n recursion call 
+//each recursion call calls function 2 time
+
+//space complexity = O(n) deepest recursion stack => f(n) -> f(n-1) -> .....-> f(1) -> f(0)
