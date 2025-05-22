ae19addba5318838962253d3af6cfd5dbe0597e1
optimisticaakash <130359837+optimisticaakash@users.noreply.github.com>
2025-05-22 11:01:16 +0530

    Update Leetcode125_validPalindrome.cpp

--- Leetcode125_validPalindrome.cpp
+++ Leetcode125_validPalindrome.cpp
@@ -33,3 +33,32 @@ public:
         return true;
     }
 };
+
+//Approach 2 : recursive approach 
+class Solution {
+public:
+    bool isAlphaNum(char ch){
+        // if((ch >= '0' && ch <= '9') || (tolower(ch) >= 'a' && (tolower(ch)) <= 'z')){
+        //     return true;
+        // }
+        // return false;
+        return isalnum(ch);//Checks if character is alpha numeric 
+                            //isalnum(ch) is a C++ standard function that returns true for [0-9], [a-z], [A-Z].
+    }
+    bool isPalindromeHelper(int i ,int j, string &s){
+        if(i >= j) return true;
+
+        if(!isAlphaNum(s[i]))
+            return isPalindromeHelper(i+1 , j , s);
+
+        if(!isAlphaNum(s[j]))
+            return isPalindromeHelper(i , j-1 , s);
+        
+        if(tolower(s[i]) != tolower(s[j])) return false;
+            return isPalindromeHelper(i+1 , j-1, s);
+    }
+    bool isPalindrome(string s) {
+        int n = s.size();
+        return isPalindromeHelper(0 , n-1 ,s);
+    }
+};
