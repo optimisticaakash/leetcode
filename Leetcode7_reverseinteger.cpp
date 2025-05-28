ac95087bae0546cdf61d710b4189efded158cc16
optimisticaakash <130359837+optimisticaakash@users.noreply.github.com>
2025-05-28 11:56:34 +0530

    Update Leetcode7_reverseinteger.cpp

--- Leetcode7_reverseinteger.cpp
+++ Leetcode7_reverseinteger.cpp
@@ -25,3 +25,23 @@ public:
 
     }
 };
+//Approach 2 : by checking overflow before multiply by 10 
+class Solution {
+public:
+    int reverse(int x) {
+        int rev = 0;
+        while(x != 0){
+            int digit = x%10;
+            x/=10;
+
+            //check for overflow before multiply by 10 
+            if(rev > INT_MAX/10 || (rev == INT_MAX/10 && digit > 7)) return 0;
+            if(rev < INT_MIN/10 || (rev == INT_MIN/10 && digit < -8)) return 0;
+
+            rev = rev *10 + digit;
+
+        }
+        return rev;
+    }
+};
+
