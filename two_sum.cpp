4227064778e180b7734c18bcf698bb5d960b97cd
optimist_aakash <130359837+optimisticaakash@users.noreply.github.com>
2025-06-05 14:06:28 +0530

    Leetcode_1 two_sum.cpp

--- two_sum.cpp
+++ two_sum.cpp
@@ -1,7 +1,9 @@
 //Two Sum
+
+//Approach 1 
 // 1. non sorted hai 
 
-// method 1 : brute force 
+// method 1 : brute force (my code )
 //Time Complexity : O(n^2) 
 class Solution {
 public:
@@ -24,10 +26,30 @@ public:
     }
 };
 
+//Alternative code :  without using extra space
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {//O(N^2)
+        int n = nums.size();
+        for(int i = 0; i < n; i++) {
+            int diff = target - nums[i];
+            for(int j = i + 1; j < n; j++) {
+                if(nums[j] == diff) {
+                    return {i, j};
+                }
+            }
+        }
+        return {}; // no pair found
+    }
+};
 
-//optimised approach : using hashMap 
-//Time complexity : O(n) 
 
+
+
+
+// approach 2 (better approach ): using hashMap 
+//Time complexity : O(nlogn) 
+//Space complexity : O(n)
 class Solution {
     public:
         vector<int> twoSum(vector<int>& nums, int target) {
@@ -48,3 +70,32 @@ class Solution {
             return {};//yhan tk nhi pahuchega kyonki answer exist krta hia
         }
     };
+//Approach 3 : if hashing and not allowed ,better than brute
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        //if we dont  cannot use map then better approach then O(n^2)
+
+        vector<pair<int,int>> v;//{value,index}//On)
+        for(int i = 0; i < nums.size(); i++){//O(n)
+            v.push_back({nums[i],i});
+        }
+
+        sort(v.begin() , v.end());
+        int left = 0; int right = nums.size()-1;
+        while(left < right){//O(n)
+            int sum = v[left].first + v[right].first;
+            if(sum == target){
+                return {v[left].second , v[right].second};
+            }
+            else if(sum > target) right--;
+            else left++;
+        }
+        return {-1,-1};
+    }
+
+    
+};
+
+//T.C = O(nLogN)
+//s.c = O(n) 
