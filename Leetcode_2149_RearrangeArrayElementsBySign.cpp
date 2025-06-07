dd9282a28116ec517993d4bf676ed8e4a8ab19c1
optimist_aakash <130359837+optimisticaakash@users.noreply.github.com>
2025-06-07 20:03:44 +0530

    Create Leetcode_2149_RearrangeArrayElementsBySign.cpp

---
+++ Leetcode_2149_RearrangeArrayElementsBySign.cpp
@@ -0,0 +1,91 @@
+//Approach1 : Bruteforce
+//Code : 
+class Solution {
+public:
+    vector<int> rearrangeArray(vector<int>& nums) {
+    int n = nums.size();
+    vector<int> pos;
+    vector<int> neg;
+    for(int i = 0; i < n; i++){
+        if(nums[i]>0){
+            pos.push_back(nums[i]);
+        }else{
+            neg.push_back(nums[i]);
+        }
+    }
+    
+    for(int i = 0; i < n/2;i++){
+        nums[2*i] = pos[i];
+        nums[2*i+1] = neg[i];
+    }
+    return nums;
+    }
+};
+//T.C: O(2n)
+//S.C : O(n)
+
+//Alternative Code :
+class Solution {
+public:
+    vector<int> rearrangeArray(vector<int>& nums) {
+    int n = nums.size();
+    vector<int> pos;
+    
+    vector<int> neg;
+    
+
+    for(int i = 0; i < n; i++){
+        if(nums[i]>0){
+            pos.push_back(nums[i]);
+        }else{
+            neg.push_back(nums[i]);
+        }
+    }
+    int a = pos.size();
+    int b = neg.size();
+
+    int i = 0;
+    int j = 0;
+    vector<int> result;
+    while(i < a || j < b){
+        if(i < a){
+            result.push_back(pos[i]);
+            i++;
+        }
+        if(j < b){
+            result.push_back(neg[j]);
+            j++;
+        }
+    }
+
+    return result;
+    }
+};
+//T.C = O(2n)
+//S.C : O(2n)
+
+//Approach 2 : optimal in single pass
+
+class Solution {
+public:
+    vector<int> rearrangeArray(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> result(n,0);
+
+        int posIndex = 0 , negIndex = 1;
+
+        for(int i = 0; i< n; i++){
+            if(nums[i] > 0){
+                result[posIndex] = nums[i];
+                posIndex += 2;
+            }else{
+                result[negIndex] = nums[i];
+                negIndex += 2;
+            }
+        }
+        return result;
+
+    }
+};
+//T.C = O(n)
+//S.C = O(n) 
