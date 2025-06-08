81a8f6cd3c0d26100a11840a9eb1dade97f9068d
optimist_aakash <130359837+optimisticaakash@users.noreply.github.com>
2025-06-08 11:16:40 +0530

    Update Leetcode_2149_RearrangeArrayElementsBySign.cpp

--- Leetcode_2149_RearrangeArrayElementsBySign.cpp
+++ Leetcode_2149_RearrangeArrayElementsBySign.cpp
@@ -89,3 +89,56 @@ public:
 };
 //T.C = O(n)
 //S.C = O(n) 
+
+//Varient 2 : pos != neg 
+//1. Place all the elements alternately, starting with a positive no, and put the remaining elements in the last
+
+//code
+// User function template for C++
+class Solution {
+  public:
+    void rearrange(vector<int> &arr) {
+        // code here
+        int n = arr.size();
+        vector<int> pos , neg;
+        // seperating pos and neg element 
+        
+        for(int i = 0; i<n; i++){
+            if(arr[i] >= 0) pos.push_back(arr[i]);
+            else neg.push_back(arr[i]);
+        }
+        //now filling them back into the array
+        //if positive more
+        if(pos.size() > neg.size()){
+            
+            for(int i = 0; i < neg.size(); i++){
+                arr[2*i] = pos[i];
+                arr[2*i+1]= neg[i];
+            }
+            //putting element which remaining in positive
+            int index = 2*(neg.size());
+            for(int i = neg.size(); i < pos.size(); i++){
+                arr[index] = pos[i];
+                index++;
+            }
+        }else{
+            //if negative more 
+            for(int i = 0; i < pos.size(); i++){
+                arr[2*i] = pos[i];
+                arr[2*i+1]= neg[i];
+            }
+            //putting element which remaining in positive
+            int index = 2*(pos.size());
+            for(int i = pos.size(); i < neg.size(); i++){
+                arr[index] = neg[i];
+                index++;
+            }
+        }
+    }
+};
+
+//T.C = O(2n)
+//S.C = O(n) 
+
+
+
