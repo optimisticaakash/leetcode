adc9f4203da07f6cf9fb8739ec64bbf5ed9bbce3
optimist_aakash <130359837+optimisticaakash@users.noreply.github.com>
2025-08-07 03:40:06 +0530

    Create LeetCode_3146_Permutation_difference_between_2strings.cpp

---
+++ LeetCode_3146_Permutation_difference_between_2strings.cpp
@@ -0,0 +1,75 @@
+//Approach1: using character hashing using 2vector
+class Solution {
+public:
+    int findPermutationDifference(string s, string t) {
+        vector<int> sindex(26,0);
+        vector<int> tindex(26,0);
+
+        for(int i = 0; i < s.size(); i++){
+            sindex[s[i]-'a'] = i;
+        }
+
+        for(int i = 0; i < s.size(); i++){
+            tindex[t[i]-'a'] = i;
+        }
+
+        int sum = 0;
+        for(char ch : s){
+            sum += abs(sindex[ch-'a'] - tindex[ch-'a']);
+        }
+
+
+        return sum;
+    } 
+};
+//T.C : O(n)
+//S.C : O(1) 
+
+//approach 2 : using only 1 vector
+class Solution {
+public:
+    int findPermutationDifference(string s, string t) {
+        //Approach 2 : using single array
+        vector<int> index(26,0);
+
+        for(int i = 0; i < s.size(); i++){
+            index[s[i]-'a'] = i;
+        }
+
+        int sum = 0;
+        for(int i = 0; i < t.size(); i++){
+            sum += abs(i - index[t[i]-'a']);
+        }
+
+
+        return sum;
+    } 
+};
+//T.C : O(n)
+//S.C : O(1)
+
+//Approach 3 : using map using for all character 
+class Solution {
+public:
+    int findPermutationDifference(string s, string t) {
+        //Approach 2 : using map
+        unordered_map<char , int> sindex;
+
+        for(int i = 0; i < s.size(); i++){
+            sindex[s[i]] = i;
+        }
+
+        int sum = 0;
+        for(int i = 0; i < t.size(); i++){
+            sum += abs(i - sindex[t[i]]);
+        }
+
+
+        return sum;
+    } 
+};
+//T.C : O(n)
+//S.C : O(1) 
+
+//if there are character more than lowercaase character then it would have worst case space complexity is O(N)
+
