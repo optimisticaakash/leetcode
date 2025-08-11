e84aad9e07f97817cd45a19c05e243973d140237
optimist_aakash <130359837+optimisticaakash@users.noreply.github.com>
2025-08-11 11:47:30 +0530

    Update LeetCode_8_implementatoi.cpp

--- LeetCode_8_implementatoi.cpp
+++ LeetCode_8_implementatoi.cpp
@@ -73,3 +73,45 @@ public:
 
 //T.C : O(n)
 //S.C: O(1)
+
+
+//Approach 3 : using recursion 
+class Solution {
+public:
+    int solveAtoi(string& s , int i , long long num ,int sign){
+        int n = s.size();
+        //BAse case
+        if(i >= n  || !isdigit(s[i])) return num*sign;
+
+        //3.recursion conver char to digit
+        int digit = s[i]-'0';
+        
+
+        if(sign == 1){
+            if(num > INT_MAX/10 || (num == INT_MAX/10 && digit > 7)) return INT_MAX;
+        }else if(sign == -1){
+            if(num > INT_MAX/10 || (num == INT_MAX/10 && digit > 8)) return INT_MIN;
+        }
+        
+        num = num*10 + digit;
+        return solveAtoi(s , i+1 , num , sign);
+    }
+    int myAtoi(string s) {
+        int i = 0; //starting index 
+        int n = s.size();
+        int sign = 1;
+        long long num = 0;
+        // 1. skip leading spaces 
+        while(i  < n && s[i]==' ') i++;
+
+        //2.handle + and -
+        if(i < n && (s[i] == '+' || s[i] == '-')){
+            sign = (s[i] == '-') ? -1:1;
+            i++;
+        }
+
+        return solveAtoi(s ,i , num ,sign);
+    }
+};
+//T.C : O(n)
+//S.C : O(n) 
