aa5270d7a9d7af5ad0bc11559a2b44b4a962b09a
optimist_aakash <130359837+optimisticaakash@users.noreply.github.com>
2025-09-19 11:28:19 +0530

    Update LeetCode_503_next_greater_element_2.cpp

--- LeetCode_503_next_greater_element_2.cpp
+++ LeetCode_503_next_greater_element_2.cpp
@@ -35,3 +35,79 @@ public:
 
 //T.C : O(N2) 
 //S.C :O(N) 
+
+
+//alternative code: bruteforce using circular array concept
+class Solution {
+public:
+    vector<int> nextGreaterElements(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> result(n,-1);
+
+        for(int i = 0; i < n; i++){
+            for(int j = 1; j < n; j++){
+                int idx = (i+j)%n;
+                if(nums[idx] > nums[i]){
+                    result[i]= nums[idx];
+                    break;
+                }
+
+            }
+        }
+
+        return result;
+    }
+};
+//T.C :O(n2)
+//S.C :(n)
+
+//Approach2 : using circular array concept doubleing the array striver
+class Solution {
+public:
+    vector<int> nextGreaterElements(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> result(n,-1);
+
+        for(int i = 0; i < n; i++){
+            for(int j = i+1; j < i+n;j++){
+                int ind = j%n;
+                if(nums[ind] > nums[i]){
+                    result[i] = nums[ind];
+                    break;
+                }
+            }
+        }
+        return result;
+    }
+};
+
+//T.C :O(N2)
+//S.C :(N)
+
+//Approach 3 : optimisted Approach (using stack) 
+class Solution {
+public:
+    vector<int> nextGreaterElements(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> result(n , -1);
+
+        stack<int> st;
+
+        for(int i = 2*n -1 ; i >= 0; i--){
+            while(!st.empty() && st.top() <= nums[i%n]){
+                st.pop();
+            }
+            
+            //number jinke nges chahiye
+            if(i < n){
+                result[i] = st.empty() ? -1 : st.top();
+            }
+            st.push(nums[i%n]);
+        }
+
+        return result;
+    }
+};
+//T.c :O(4N) 
+//S.C :O(2N) + O(N)
+
