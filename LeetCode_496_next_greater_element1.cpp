7052d467f58b5ae3e41ffbd88e9e47b9677a450b
optimist_aakash <130359837+optimisticaakash@users.noreply.github.com>
2025-09-19 11:23:35 +0530

    Create LeetCode_496_next_greater_element1.cpp

---
+++ LeetCode_496_next_greater_element1.cpp
@@ -0,0 +1,30 @@
+//Approach : using stack and map 
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        unordered_map<int,int> mpp;
+
+        stack<int> st;
+        int n = nums2.size();
+
+        for(int i = n-1; i >= 0; i--){
+            while(!st.empty() && st.top() <= nums2[i]){
+                st.pop();
+            }
+            if(st.empty()) mpp[nums2[i]] = -1;
+            else mpp[nums2[i]] = st.top();
+
+            st.push(nums2[i]);
+        }
+        vector<int> ans;
+
+        for(int i = 0; i < nums1.size(); i++){
+            ans.push_back(mpp[nums1[i]]);
+        }
+
+        return ans;
+    }
+};
+
+//T.C :O(N + M)
+//S.C : O(N) + O(M) 
